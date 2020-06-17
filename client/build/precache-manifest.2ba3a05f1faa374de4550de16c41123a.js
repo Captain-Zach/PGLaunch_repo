0db6ec1a61472cbc2a03453a54a43a6fc4ca62a1
Zach Jones <zacharytjones1@gmail.com>
2020-06-17 12:25:30 -0500

    run build activated

---
+++ client/build/precache-manifest.2ba3a05f1faa374de4550de16c41123a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5bc7eca3d4844b80e5cada33ccd066c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b442fd7cf5c5ca97e06",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "f5e890890c9ff48dcd11",
+    "url": "/static/js/2.3a14709a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.3a14709a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b442fd7cf5c5ca97e06",
+    "url": "/static/js/main.486b5ac0.chunk.js"
+  },
+  {
+    "revision": "08a01cac7a9424eef63f",
+    "url": "/static/js/runtime-main.ef76e641.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
